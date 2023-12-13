b65ee12480b5c35d05932886a064d86e1c5749df
AmadorUzumaki <72940615+AmadorUzumaki@users.noreply.github.com>
2023-12-13 20:40:07 +0100

    Mejorando el expositor

--- main.js
+++ main.js
@@ -9,42 +9,43 @@ const scene = new THREE.Scene()
 
 //crear ratio per la càmera
 const fov = 60
-const aspect = window.innerWidth/window.innerHeight
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+const aspect = sizes.width / sizes.height
 const near = 0.1
 const far = 1000
 
 //crear el renderer
 const renderer = new THREE.WebGLRenderer()
 document.body.appendChild(renderer.domElement)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 //crear el cubemap
 
 const cubeTextureLoader = new THREE.CubeTextureLoader()
 const environmentMap = cubeTextureLoader.load([
-  'textures/environmentMaps/Studio/px.png',
-  'textures/environmentMaps/Studio/nx.png',
-  'textures/environmentMaps/Studio/py.png',
-  'textures/environmentMaps/Studio/ny.png',
-  'textures/environmentMaps/Studio/pz.png',
-  'textures/environmentMaps/Studio/nz.png'
+    'textures/environmentMaps/Studio/px.png',
+    'textures/environmentMaps/Studio/nx.png',
+    'textures/environmentMaps/Studio/py.png',
+    'textures/environmentMaps/Studio/ny.png',
+    'textures/environmentMaps/Studio/pz.png',
+    'textures/environmentMaps/Studio/nz.png'
 ])
 
 scene.background = environmentMap;
 
 //fer que la pestanya es torni de la mida que és l'explorador actualment
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    renderer.setSize(window.innerWidth,window.innerHeight)
+    renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
@@ -61,19 +62,42 @@ const albedoFloorTexture = textureLoader.load(albedoFloor)
 const normalFloorTexture = textureLoader.load(normalFloor)
 const ARMFloorTexture = textureLoader.load(ARMFloor)
 
-//crear el raycast que canviarà la mida y rotarà els objectes
-const raycaster = new THREE.Raycaster()
+//objectes buits per posar els models damunt
+const Link = new THREE.Object3D();
+Link.position.x = -25;
+scene.add(Link);
+const Tharja = new THREE.Object3D();
+scene.add(Tharja);
+const Yopuka = new THREE.Object3D();
+Yopuka.position.x = 25;
+scene.add(Yopuka);
+
+//punts HTML
+const points = [
+    {
+        position: new THREE.Vector3(1.55, 0.3, - 0.6),
+        element: document.querySelector('.point-0')
+    }
+]
+
+
 //crear la constant del ratolí que utilitzarem per guardar la posició d'aquest
 const mouse = new THREE.Vector2()
+mouse.x = -1
+mouse.y = -1
 
 //guardar la posició del ratolí
-window.addEventListener('mousemove', (event) =>{
+window.addEventListener('mousemove', (event) => {
     mouse.x = event.clientX / sizes.width * 2 - 1
     mouse.y = - (event.clientY / sizes.height) * 2 + 1
-    console.log(mouse)
+    //console.log(mouse)
 })
 
-const clock = new THREE.Clock()
+window.addEventListener('click', () => {
+    console.log('click')
+    })
+
+//const clock = new THREE.Clock()
 
 //crear una superfície per posar els objectes, amb una geometria, el material a partir de les imatges, y la rotació adequada per que sigui en terra
 const planeGeo = new THREE.PlaneGeometry(100, 100)
@@ -83,8 +107,8 @@ const planeMat = new THREE.MeshStandardMaterial({
     aoMap: ARMFloorTexture,
     roughnessMap: ARMFloorTexture,
     metalnessMap: ARMFloorTexture
-  
-  });
+
+});
 
 const plane = new THREE.Mesh(planeGeo, planeMat)
 plane.rotation.x = Math.PI * -0.5
@@ -98,7 +122,7 @@ camera.position.set(0, 15, 30);
 camera.lookAt(0, 0, 0);
 
 //cub de prova
-const cubeMat = new THREE.MeshStandardMaterial({color: 0xFF00FF});
+const cubeMat = new THREE.MeshStandardMaterial({ color: 0xFF00FF });
 const cubeGeo = new THREE.BoxGeometry();
 const cube = new THREE.Mesh(cubeGeo, cubeMat);
 cube.position.y = 0.5;
@@ -107,14 +131,39 @@ cube.position.y = 0.5;
 //constant per tal de poder moure la càmera
 const controls = new OrbitControls(camera, renderer.domElement)
 
-//spotlight per il·luminar l'escena
-const SpotLight = new THREE.SpotLight({color: 0x00FF00, intensity: 10000, angle: Math.PI/8})
-SpotLight.position.y = 40;
+//spotlight de Link
+const spotLightLink = new THREE.SpotLight(0xFFFFFF, 10000, 0, Math.PI / 8)
+spotLightLink.position.x = -25;
+spotLightLink.position.y = 40;
+spotLightLink.target = Link;
 
-const SpotLightHelper = new THREE.SpotLightHelper(SpotLight);
+const spotLightHelperLink = new THREE.SpotLightHelper(spotLightLink);
+spotLightLink.visible = false;
+scene.add(spotLightLink);
+scene.add(spotLightHelperLink);
 
-scene.add(SpotLight);
-scene.add(SpotLightHelper);
+//spotlight de Tharja
+
+const spotLightTharja = new THREE.SpotLight(0xFFFFFF, 10000, 0, Math.PI / 8)
+spotLightTharja.position.y = 40;
+spotLightTharja.target = Tharja;
+
+const spotLightHelperTharja = new THREE.SpotLightHelper(spotLightTharja);
+spotLightTharja.visible = false;
+scene.add(spotLightTharja);
+scene.add(spotLightHelperTharja);
+
+//spotlight de Yopuka
+
+const spotLightYopuka = new THREE.SpotLight(0xFFFFFF, 10000, 0, Math.PI / 8)
+spotLightYopuka.position.x = 25;
+spotLightYopuka.position.y = 40;
+spotLightYopuka.target = Yopuka;
+
+const spotLightHelperYopuka = new THREE.SpotLightHelper(spotLightYopuka);
+spotLightYopuka.visible = false;
+scene.add(spotLightYopuka);
+scene.add(spotLightHelperYopuka);
 
 //llum ambiental per poder veure fins que tot funcioni
 const ambientLight = new THREE.AmbientLight(0xfffff, 1)
@@ -122,48 +171,53 @@ const ambientLight = new THREE.AmbientLight(0xfffff, 1)
 scene.add(ambientLight);
 
 //importar models a l'escena, col·locar-los i redimensionar-los
-ImportModel("models/wakfu_fanart_-_female_iop/scene.gltf", 25, 0.25)
-ImportModel("models/ocarina_of_time_link/scene.gltf", -25, 0.75)
-ImportModel("models/sallya/scene.gltf", 0, 6)
+ImportModel("models/wakfu_fanart_-_female_iop/scene.gltf", Yopuka, 0.25)
+ImportModel("models/ocarina_of_time_link/scene.gltf", Link, 0.75)
+ImportModel("models/sallya/scene.gltf", Tharja, 6)
 
 //temps per tener un loop d'animació
 let Time = Date.now()
 
-//variable per poder util·litzar per importar models
-let model = null;
+let model = null
 
 //funció de loop d'animació que servirà per fer que els objectes necessaris tenguin animació
 AnimationLoop()
 
-function AnimationLoop(){
+function AnimationLoop() {
     let ThisTime = Date.now()
     let deltaTime = ThisTime - Time
-    Time = ThisTime;
+    Time = ThisTime
+    for(const point of points)   {
+        const screenPosition = point.position.clone()
+        screenPosition.project(camera)
+        console.log(screenPosition.x)
+
+    }
     renderer.render(scene, camera)
     requestAnimationFrame(AnimationLoop)
 }
 //crear una funció per poder importar models
-function ImportModel(route, xLocation, scalated){
+function ImportModel(route, object3D, scalated) {
 
     const LoaderModel = new GLTFLoader();
 
     LoaderModel.load
-    (
-        route,
+        (
+            route,
 
-        function (gltf) {
-            model = gltf.scene;
-            scene.add(model);
-            model.position.set(xLocation, 0, 0);
-            model.scale.set(scalated, scalated, scalated);
-        },
-        function(xhr){
-            console.log ((xhr.loaled / xhr.total * 100)+ "%loaded")
-        },
-        function (error) {
-            //callback per quan hi ha un error. El podem mostrar per consola.
-            console.error(error);
-        }
-    )
+            function (gltf) {
+                //console.log("hola");
+                model = gltf.scene;
+                object3D.add(model);
+                model.scale.set(scalated, scalated, scalated);
+            },
+            function (xhr) {
+                //console.log((xhr.loaled / xhr.total * 100) + "%loaded")
+            },
+            function (error) {
+                //callback per quan hi ha un error. El podem mostrar per consola.
+                //console.error(error);
+            }
+        )
 
 }
